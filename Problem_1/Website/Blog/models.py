40e81058ba406df20b1d7f90a91c1acd03e3cf47
Kunal <kunaltyagi760@gmail.com>
2024-07-06 13:14:56 +0530

    Problem 3 solved

--- Problem_1/Website/Blog/models.py
+++ Problem_1/Website/Blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+    postId = models.IntegerField()
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- Problem_1/Website/Blog/views.py
+++ Problem_1/Website/Blog/views.py
@@ -1,19 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
 
-posts = [
-    {'id': 1, 'title': 'First Post', 'content': 'This is the first post.'},
-    {'id': 2, 'title': 'Second Post', 'content': 'This is the second post.'},
-    {'id': 3, 'title': 'Third Post', 'content': 'This is the third post.'},
-]
+# posts = [
+#     {'id': 1, 'title': 'First Post', 'content': 'This is the first post.'},
+#     {'id': 2, 'title': 'Second Post', 'content': 'This is the second post.'},
+#     {'id': 3, 'title': 'Third Post', 'content': 'This is the third post.'},
+# ]
+
+posts = models.Posts.objects.all()
 
 def post_list(request):
     context = {'posts': posts}
     return render(request, 'Blog/post_list.html', context)
 
 def post_detail(request, id):
-    post = next((post for post in posts if post['id'] == id), None)
+
+    # When post is present in views.py
+    # post = next((post for post in posts if post['id'] == id), None)
+    # context = {'post': post}
+    # return render(request, 'Blog/post_detail.html', context)
+
+
+    # Method-1 To get a post from models and display it.
+    # context = {'post': posts[id-1]}
+    # return render(request, 'Blog/post_detail.html', context)
+
+
+    # Method-2 To get a post from models and display it.
+    post = get_object_or_404(posts,id=id)
     context = {'post': post}
     return render(request, 'Blog/post_detail.html', context)
+
+
+
 
 
